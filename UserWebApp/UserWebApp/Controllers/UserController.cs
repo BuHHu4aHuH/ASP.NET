3f5d4cee259a2cf1c6c4c2c41c4a06346d19465f
Maksim Shershun <maksshershun95@gmail.com>
2019-01-12 20:54:48 +0200

    added delete

--- UserWebApp/UserWebApp/Controllers/UserController.cs
+++ UserWebApp/UserWebApp/Controllers/UserController.cs
@@ -42,6 +42,12 @@
             return this.View(list);
         }
 
+        /// <summary>
+        /// The create.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
         public ActionResult Create()
         {
             this.ViewBag.InfoForPartial = "Create new";
@@ -49,5 +55,116 @@
             return this.View();
         }
 
+        /// <summary>
+        /// The create.
+        /// </summary>
+        /// <param name="dto">
+        /// The dto.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        [HttpPost]
+        public ActionResult Create(UserDto dto)
+        {
+            this.ViewBag.InfoForPartial = "Create error";
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(dto);
+            }
+
+            var model = MapToItem(dto);
+
+            this.context.Items.Add(model);
+            this.context.SaveChanges();
+
+            return this.RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// The delete.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Delete(int id)
+        {
+            var item = this.context.Items.Find(id);
+
+            if (item == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            var dto = MapToDto(item);
+
+            return this.View(dto);
+        }
+
+        /// <summary>
+        /// The delete post.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeletePost(int id)
+        {
+            var item = this.context.Items.Find(id);
+
+            if (item == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            this.context.Items.Remove(item);
+            this.context.SaveChanges();
+
+            return this.RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// The map to item.
+        /// </summary>
+        /// <param name="dto">
+        /// The dto.
+        /// </param>
+        /// <returns>
+        /// The <see cref="UserItem"/>.
+        /// </returns>
+        private static UserItem MapToItem(UserDto dto) =>
+        new UserItem
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                birthdayData = dto.dateOfBirthday
+            };
+
+        /// <summary>
+        /// The map to dto.
+        /// </summary>
+        /// <param name="item">
+        /// The item.
+        /// </param>
+        /// <returns>
+        /// The <see cref="UserDto"/>.
+        /// </returns>
+        private static UserDto MapToDto(UserItem item) =>
+        new UserDto
+        {
+                Id = item.Id,
+                Name = item.Name,
+                dateOfBirthday = item.birthdayData
+        };
     }
 }
+
+
